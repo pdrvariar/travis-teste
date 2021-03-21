27dc84f564eb7b42ba71825e2c0e201c731b8be1
Pablo Rattes <pdrvariar@gmail.com>
2021-03-21 18:22:20 -0300

    Versão Inicial

---
+++ src/main/java/com/tpi/api/TravisTesteApplication.java
@@ -0,0 +1,13 @@
+package com.tpi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisTesteApplication.class, args);
+	}
+
+}
